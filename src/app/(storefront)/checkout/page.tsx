c4b6541962e0a3917cdecbf07b087dfcc7f2941a
Huynh Minh Nhut <hminhnhut0501@gmail.com>
2026-07-23 16:46:59 +0700

    Prefer bot checkout params on web

--- src/app/(storefront)/checkout/page.tsx
+++ src/app/(storefront)/checkout/page.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Link from "next/link";
 import { CheckoutPaymentForm } from "@/components/storefront/checkout-payment-form";
 import { getStorefrontLocale } from "@/modules/i18n/storefront";
